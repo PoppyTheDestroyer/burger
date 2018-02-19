c15bf5b2145db88854ac4b00794d16c05cd28a3b
Jason Saenz <jsigns@gmail.com>
2018-02-19 14:05:59 -0600

    deploy

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,15 @@
 const mysql = require("mysql");
-
-const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "burgers_db"
-});
+var connection;
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "burgers_db"
+  });
+}
 
 connection.connect(err => {
   if (err) {
